20b6502fab0f9f44d6af62235e5344fe764ea998
roman <roman3668@gmail.com>
2022-11-23 12:06:23 +0300

    Add method GetShortWordArray

--- Program.cs
+++ Program.cs
@@ -6,3 +6,26 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+string[] arr1 = { "Hello", "2", "world", ":-)" };
+string[] arr2 = { "1234", "1567", "-2", "computer science" };
+string[] arr3 = { "Russia", "Denmark", "Kazan" };
+int maxWordLength = 3;
+
+string[] GetShortWordArray(string[] array)
+{
+    int length = array.Length;
+    string[] result = new string[length];
+    int count = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i].Length <= maxWordLength)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref result, count);
+
+    return result;
+}
